45198477c2d288eb5022eebb680082286f669962
Alexei_Nech <alexei_93@yahoo.com>
2012-08-23 19:37:50 +0300

    Update Lua-Scripts/AkMi_Lua_Stone.lua

--- Lua-Scripts/AkMi_Lua_Stone.lua
+++ Lua-Scripts/AkMi_Lua_Stone.lua
@@ -1,4 +1,8 @@
---CREDITS AK47SIGH (aka AlexeWarr) & MidoKing
+-- Project Maintained by Ak47sigh known as Alexe & Midoking.
+-- You can redistribute it and/or modify but not to sell.
+-- Actions against the Copyright will support consequences!
+-- Copyright (C) 2012-2013 AkMi Project <http://www.wow-like.info/>
+
 
 local itemid = 6948 -- your item ID
 
