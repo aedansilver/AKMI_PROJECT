016cdf32e5f47a4f100f9cf667aa50af92b0f10a
Alexei_Nech <alexei_93@yahoo.com>
2012-08-23 19:39:31 +0300

    Update Lua-Scripts/Autobroadcast.lua

--- Lua-Scripts/Autobroadcast.lua
+++ Lua-Scripts/Autobroadcast.lua
@@ -1,5 +1,12 @@
+-- Project Maintained by Ak47sigh known as Alexe & Midoking.
+-- You can redistribute it and/or modify but not to sell.
+-- Actions against the Copyright will support consequences!
+-- Copyright (C) 2012-2013 AkMi Project <http://www.wow-like.info/>
+
+-- this script will be changed soon for better purpose
+
 function World_AutoAnnounce()
-	RandomAnnounce=math.random(1,2) -- Change it if you gonna add more.
+	RandomAnnounce = math.random(1,2) -- Change it if you gonna add more.
 		if RandomAnnounce==1 then
 			SendWorldMessage("|cffff0000[System Broadcast] |cff00ff00You can use /o to talk in the World Channel.", 2) -- 1 For Chat Box and 2 For WideScreen.
 		end
