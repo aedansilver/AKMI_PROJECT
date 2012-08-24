a7f77576acc1ea0c651ed89ff58b49fd3ee9ca74
ak47sigh <alexei_93yahoo.com>
2012-08-24 15:55:24 +0300

    add core modification : show mute in world trinity core

---
+++ Core Modifications/Show_Mute_In_World.cpp
@@ -0,0 +1,72 @@
+ /*
+Project Maintained by Ak47sigh known as Alexe & Midoking.
+You can redistribute it and/or modify but not to sell.
+Actions against the Copyright will support consequences!
+Copyright (C) 2012-2013 AkMi Project <http://www.wow-like.info/>
+*/
+
+/* HOW TO INSTALL:
+Open cs_misc.cpp file from your project solution->scripts->..
+Search for :
+--]]__
+    static bool HandleMuteCommand(ChatHandler* handler, char const* args)
+    {
+        char* nameStr;
+        char* delayStr;
+        handler->extractOptFirstArg((char*)args, &nameStr, &delayStr);
+        if (!delayStr)
+            return false;
+
+        char const* muteReason = strtok(NULL, "\r");
+        std::string muteReasonStr = "No reason";
+        if (muteReason != NULL)
+            muteReasonStr = muteReason;
+
+        Player* target;
+        uint64 targetGuid;
+        std::string targetName;
+        if (!handler->extractPlayerTarget(nameStr, &target, &targetGuid, &targetName))
+            return false;
+
+        uint32 accountId = target ? target->GetSession()->GetAccountId() : sObjectMgr->GetPlayerAccountIdByGUID(targetGuid);
+
+        // find only player from same account if any
+        if (!target)
+            if (WorldSession* session = sWorld->FindSession(accountId))
+                target = session->GetPlayer();
+
+        uint32 notSpeakTime = uint32(atoi(delayStr));
+
+        // must have strong lesser security level
+        if (handler->HasLowerSecurity (target, targetGuid, true))
+            return false;
+
+        PreparedStatement* stmt = LoginDatabase.GetPreparedStatement(LOGIN_UPD_MUTE_TIME);
+
+        if (target)
+        {
+            // Target is online, mute will be in effect right away.
+            int64 muteTime = time(NULL) + notSpeakTime * MINUTE;
+            target->GetSession()->m_muteTime = muteTime;
+            stmt->setInt64(0, muteTime);
+            ChatHandler(target).PSendSysMessage(LANG_YOUR_CHAT_DISABLED, notSpeakTime, muteReasonStr.c_str());
+        }
+        else
+        {
+            // Target is offline, mute will be in effect starting from the next login.
+            int32 muteTime = -int32(notSpeakTime * MINUTE);
+            stmt->setInt64(0, muteTime);
+        }
+
+        stmt->setUInt32(1, accountId);
+        LoginDatabase.Execute(stmt);
+        std::string nameLink = handler->playerLink(targetName);
+
+        handler->PSendSysMessage(target ? LANG_YOU_DISABLE_CHAT : LANG_COMMAND_DISABLE_CHAT_DELAYED, nameLink.c_str(), notSpeakTime, muteReasonStr.c_str());
+...................................
+Add after line:
+}
+*/
+handler->PSendSysMessage(target ? LANG_YOU_DISABLE_CHAT : LANG_COMMAND_DISABLE_CHAT_DELAYED, nameLink.c_str(), notSpeakTime, muteReasonStr.c_str());
+
++ sWorld->SendWorldText(12102, nameLink.c_str(), handler->GetSession()->GetPlayerName(), notSpeakTime, muteReasonStr.c_str());
